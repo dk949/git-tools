82c1ad9334e3a2e36db980bf0e79f6cf101d210a
dk949 <56653556+dk949@users.noreply.github.com>
2023-09-29 23:29:22 +0100

    forwarding arguments to git log in git log-q

--- git-log-q.sh
+++ git-log-q.sh
@@ -1,1 +1,1 @@
-git log --pretty=format:"%C(auto)%h %Cblue%ad%<(10) %Cred%an%C(auto)%d %s" --date='format:%F %R'
+git log --pretty=format:"%C(auto)%h %Cblue%ad%<(10) %Cred%an%C(auto)%d %s" --date='format:%F %R' "$@"
