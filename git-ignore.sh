dbc7f1b23021a3332a11aa02aae9c239d29958a2
dk949 <56653556+dk949@users.noreply.github.com>
2024-06-02 20:45:31 +0100

    updated git-ignore

--- git-ignore.sh
+++ git-ignore.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
-failMsg() {
-    printf "fatal: not a git repository (or any parent up to mount point \/)\nStopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set)."
+
+usage() {
+    echo "Usage: $(basename "$0") PATH [LOC]
+    PATH            file to ignore
+    LOC             location of the gitignore file (local|base)
+"
+    exit "$1"
+}
+
+echo " $* " | grep " --help " -q || echo " $* " | grep " -h " -q && usage 0
+
+path="$1"
+[ -z "$path" ] && usage 1
+loc=base
+[ -n "$2" ] && loc="$2"
+
+dot_git_dir="$(git rev-parse --git-dir 2> /dev/null)" || {
+    echo "Not a git directory"
     exit 1
 }
 
-CURRENT=$PWD
-GIT="$(git rev-parse --git-dir 2> /dev/null)" || failMsg
-GIT=$(cd "$GIT/.." || exit 1; pwd)
-cd "$GIT" || exit 1
+base_dir=$(realpath "$dot_git_dir"/..)
 
-if [ -z "${CURRENT#$GIT}" ]; then
-    echo "$1" >> ./.gitignore
-else
-    echo "${CURRENT#$GIT}"/"$1" >> ./.gitignore
-fi
+case $loc in
+    base) work_dir=$base_dir;;
+    local) work_dir=$PWD;;
+    *) usage 1
+esac
 
-cd "$CURRENT" || exit 1
+[ -e "$path" ] || echo "Warning: $path does not exist, but will be added to gitignore"
+
+realpath -s --relative-to="$work_dir" "$path" >> "$work_dir/.gitignore"
